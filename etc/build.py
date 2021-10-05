ceb115bb1ff8a1ffb2452f12c6a0c2c6a0b4fcb0
yonatanh20 <yonatan.harari@gmail.com>
2021-10-05 19:47:34 +0300

    Bug fix
    corrected file names in code

--- etc/build.py
+++ etc/build.py
@@ -3,17 +3,17 @@ from pathlib import Path
 import os
 import zipfile
 
-PACKAGE_NAME="rdp"
-FILES=["Rdp.py","Rdp.ico","LICENSE"]
+PACKAGE_NAME = "rdp"
+FILES = ["rdp.py", "rdp.ico", "LICENSE"]
 
-ETC_FOLDER=Path(__file__).parent
-PACKAGE_FOLDER=ETC_FOLDER.parent
-PACKAGE_FILE=Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
+ETC_FOLDER = Path(__file__).parent
+PACKAGE_FOLDER = ETC_FOLDER.parent
+PACKAGE_FILE = Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
 
 print(f"Creating package file {PACKAGE_FILE}")
 
 try:
-    zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+    zf = zipfile.ZipFile(PACKAGE_FILE, 'w', zipfile.ZIP_DEFLATED)
     for f in FILES:
         zf.write(Path.joinpath(Path('..'), f), f)
 
@@ -26,4 +26,4 @@ except Exception as exc:
     print(f"Failed to create package {PACKAGE_NAME}. {exc}")
     os._exit(1)
 
-print(f"Done")
+print("Done")
